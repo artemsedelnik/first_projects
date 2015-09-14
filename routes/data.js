1a59b325b620c3108169c7cc81d971d90125ebca
ArtemSedelnik <sedelnik.artem@gmail.ru>
2015-09-14 13:37:02 +0300

    angular homework1

--- routes/data.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ArtemSedelnik on 11.09.15.
- */
-var fs = require('fs');
-exports.get = function(req, res, next) {
-    var persons = JSON.parse(fs.readFileSync('data/sampleJSON.json', 'utf8'));
-    res.send(persons);
-};
--- routes/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (app){
-    app.get('/', require('./home').get);
-    app.get('/data', require('./data').get);
-};
